be447770d94c4695724e19cc8f00d51b9186d6eb
Luiz Calaça <luizcalaca@gmail.com>
2023-01-25 20:24:49 -0300

    feat: adding login

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -22,9 +22,27 @@ const createUserResponseSchema = z.object({
     ...userCore
 })
 
+const loginSchema = z.object({
+    email: z
+      .string({
+        required_error: "Email is required",
+        invalid_type_error: "Email must be a string",
+      })
+      .email(),
+    password: z.string(),
+  });
+  
+  const loginResponseSchema = z.object({
+    accessToken: z.string(),
+  });
+
 export type CreateUserInput = z.infer<typeof createUserSchema>
+
+export type LoginInput = z.infer<typeof loginSchema>;
 
 export const { schemas: userSchemas, $ref } = buildJsonSchemas({
     createUserSchema,
-    createUserResponseSchema
+    createUserResponseSchema,
+    loginSchema,
+    loginResponseSchema,
 })
