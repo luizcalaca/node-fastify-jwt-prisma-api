6c21871134e3910283315f9083b3f94dc4014dfd
Luiz Calaça <luizcalaca@gmail.com>
2023-01-25 18:33:11 -0300

    feat: adding schemas to the route

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,8 +1,16 @@
 import { FastifyInstance } from "fastify";
 import { registerUserHandler } from "./user.controller";
+import { $ref } from "./user.schema";
 
 async function userRoutes(server: FastifyInstance) {
-    server.post('/', registerUserHandler)
+    server.post('/', {
+        schema: {
+            body: $ref('createUserSchema'),
+            response: {
+                201: $ref('createUserResponseSchema')
+            }
+        }
+    },registerUserHandler)
 }
 
 export default userRoutes
