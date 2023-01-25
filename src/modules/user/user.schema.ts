4b754d4e4505b197ee5213ef7c277476c315e184
Luiz Calaça <luizcalaca@gmail.com>
2023-01-25 18:32:20 -0300

    refactor: new user schema separeted

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,15 +1,30 @@
-import { z } from 'zod'
+import { z } from 'zod';
+import { buildJsonSchemas } from 'fastify-zod';
 
-const createUserSchema = z.object({
+const userCore = {
     email: z.string({
         required_error: 'Email is required',
         invalid_type_error: 'Email must be a string'
     }).email(),
     name: z.string(),
+}
+
+const createUserSchema = z.object({
+    ...userCore,
     password: z.string({
         required_error: 'Password is required',
         invalid_type_error: 'Password must be a string'
     })
 });
 
+const createUserResponseSchema = z.object({
+    id: z.number(),
+    ...userCore
+})
+
 export type CreateUserInput = z.infer<typeof createUserSchema>
+
+export const { schemas: userSchemas, $ref } = buildJsonSchemas({
+    createUserSchema,
+    createUserResponseSchema
+})
