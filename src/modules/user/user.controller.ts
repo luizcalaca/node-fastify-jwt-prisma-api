69f3820d9877536e657d92cd225e878a0d1a2ead
Luiz Calaça <luizcalaca@gmail.com>
2023-01-25 18:33:57 -0300

    feat: adding type for UserInput and import createUser

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,21 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { CreateUserInput } from "./user.schema";
+import { createUser } from "./user.service";
 
 export async function registerUserHandler(
-    request: FastifyRequest,
+    request: FastifyRequest<{
+      Body: CreateUserInput;
+    }>,
     reply: FastifyReply
-) {}
+  ) {
+    const body = request.body;
+  
+    try {
+      const user = await createUser(body);
+  
+      return reply.code(201).send(user);
+    } catch (e) {
+      console.log(e);
+      return reply.code(500).send(e);
+    }
+  }
