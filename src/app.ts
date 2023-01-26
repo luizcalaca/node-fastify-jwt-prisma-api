84c276c82e795d1a3fe74a5716627c1bbf079dbe
Luiz Calaça <luizcalaca@gmail.com>
2023-01-25 23:12:44 -0300

    refactor: app and server refactored

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,16 @@
-import Fastify, { FastifyReply, FastifyRequest } from 'fastify';
-import userRoutes from './modules/user/user.route';
-import { userSchemas } from './modules/user/user.schema';
-import fjwt, { JWT } from "@fastify/jwt";
+import buildServer from "./server";
 
-declare module "fastify" {
-    interface FastifyRequest {
-      jwt: JWT;
-    }
-    export interface FastifyInstance {
-      authenticate: any;
-    }
-}
-
-export const server = Fastify()
-
-server.register(fjwt, {
-    secret: "aowldpowierçalskd"
-})
-
-server.decorate(
-    "authenticate",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      try {
-        await request.jwtVerify();
-      } catch (e) {
-        return reply.send(e);
-      }
-    }
-  );
-
-server.get('/healthcheck', async function (request, response) {
-    return {status: "OK"}
-})
+const server = buildServer();
 
 async function main() {
+  try {
+    await server.listen({ port: 3000 });
 
-    for (const schema of userSchemas) {
-        server.addSchema(schema)
-    }
-    server.register(userRoutes, {prefix: 'api/users'})
-
-    try {
-        await server.listen({port: 3000})
-    } catch (error) {
-        console.error(error)
-        process.exit(1)
-    }
+    console.log(`Server ready at http://localhost:3000`);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 }
 
-main()
+main();
